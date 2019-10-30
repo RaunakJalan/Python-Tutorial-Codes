7b84ddc724730fb5fc845d01b7b584f984d9dc35
Raunak Jalan <41023976+RaunakJalan@users.noreply.github.com>
2019-10-30 12:26:58 +0530

    Update messageAPI.py

--- messageAPI.py
+++ messageAPI.py
@@ -2,15 +2,15 @@ from twilio.rest import Client
 
 
 
-account_sid = 'AC372ba4054aeba0e57ab832ccc00fa0bf' # Found on Twilio Console Dashboard
+account_sid = '<auth_id>' # Found on Twilio Console Dashboard
 
-auth_token = 'a1ddd43a44b336db7862f2bdda65c337' # Found on Twilio Console Dashboard
+auth_token = '<auth_token>' # Found on Twilio Console Dashboard
 
 
 
-myPhone = '+917984590915' # Phone number you used to verify your Twilio account
+myPhone = '<Phone number>' # Phone number you used to verify your Twilio account
 
-TwilioNumber = '18594708166' # Phone number given to you by Twilio
+TwilioNumber = '<Twilio_Number>' # Phone number given to you by Twilio
 
 
 
